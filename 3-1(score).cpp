2425f111dbbee9cec52a85d8935f17729e58eb57
hanwenxiu <35623465+hanwenxiu@users.noreply.github.com>
2018-01-31 08:23:17 +0800

    Rename ConsoleApplication2.cpp to 3-1(score).cpp

---
+++ 3-1(score).cpp
@@ -0,0 +1,29 @@
+﻿// ConsoleApplication2.cpp: 定义控制台应用程序的入口点。
+//
+#include<string.h>
+#include "stdafx.h"
+#include<stdio.h>
+#define maxn 105
+int main()
+{
+	char s[maxn]; int n;
+	int score=0, count=1;
+	int p = 0;
+		scanf_s("%s", s);
+		n = strlen(s);
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == 'O')
+			{
+				if (p)
+					count++;
+				score = score + count;
+				p = 1;
+			}
+			else
+				p = 0;
+		}
+		printf("%d", score);
+
+}
+
